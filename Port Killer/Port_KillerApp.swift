8ef27a2b0d3c11376aa4a151d50f7a9552493fbe
MehmetAkifff <makifergn@gmail.com>
2026-04-09 01:03:40 +0300

    Initial Commit

---
+++ Port Killer/Port_KillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Port_KillerApp.swift
+//  Port Killer
+//
+//  Created by Mehmet Akif ERGANİ on 9.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Port_KillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
